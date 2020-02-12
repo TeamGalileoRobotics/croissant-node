5bf0da5a860c6bee085f12e9308ed1a470bdf372
Galileorobotics <info@galileorobotics.ch>
2020-02-12 18:52:54 +0100

    Add basic websocket setup
    + reorganize file structure for easier deployment

---
+++ src/main.py
@@ -0,0 +1,17 @@
+import network
+import config
+import uwebsockets.client
+
+def connect(ssid = config.NETWORK_SSID, password = config.NETWORK_PASSWORD):
+    sta_if = network.WLAN(network.STA_IF)
+    if not sta_if.isconnected():
+        print('connecting to network...')
+        sta_if.active(True)
+        sta_if.connect(ssid, password)
+        while not sta_if.isconnected():
+            pass
+    print('network config:', sta_if.ifconfig())
+
+def start():
+    with uwebsockets.client.connect('ws://' + config.SERVER) as websocket:
+        websocket.send("🥐")
