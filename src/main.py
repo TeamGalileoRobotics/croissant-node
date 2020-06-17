9f8e8f042a83b0ffa1a2f75879dd7b5839a77418
Galileorobotics <info@galileorobotics.ch>
2020-06-17 19:12:29 +0200

    Add ability to read and send sensor data
    Read sensor data via SPI and send it as a tuple via websockets.
    Untested because of missing hardware.

--- src/main.py
+++ src/main.py
@@ -1,8 +1,15 @@
-import network
+import time
+
+import adafruit_lis3dh
+import board
+import busio
 import config
+import digitalio
+import network
 import uwebsockets.client
 
-def connect(ssid = config.NETWORK_SSID, password = config.NETWORK_PASSWORD):
+
+def connect(ssid=config.NETWORK_SSID, password=config.NETWORK_PASSWORD):
     sta_if = network.WLAN(network.STA_IF)
     if not sta_if.isconnected():
         print("connecting to network...")
@@ -12,9 +19,19 @@ def connect(ssid = config.NETWORK_SSID, password = config.NETWORK_PASSWORD):
             pass
     print("network config:", sta_if.ifconfig())
 
+
 def start():
+    spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
+    cs = digitalio.DigitalInOut(board.D5)  # Set to appropriate CS pin!
+    int1 = digitalio.DigitalInOut(board.D6)  # Set to correct pin for interrupt!
+    lis3dh = adafruit_lis3dh.LIS3DH_SPI(spi, cs, int1=int1)
+
     with uwebsockets.client.connect("ws://" + config.SERVER) as websocket:
-        websocket.send("🥐")
+        while True:
+            x, y, z = lis3dh.acceleration
+            websocket.send((x, y, z))
+            time.sleep(0.1)
+
 
 connect()
 start()
